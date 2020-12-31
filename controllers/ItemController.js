2a87d4490b69ac97c36f4c6e3979b3faa4a092f0
Igor Marques <soigormarques@gmail.com>
2020-12-31 11:05:26 -0300

    Update

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -5,6 +5,7 @@ class ItemController {
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
 
+
         this.onSubmit();
     }
 
@@ -20,11 +21,19 @@ class ItemController {
 
     }
 
-    getValues() {
+    getValues(formEl) {
 
         let item = {};
+        let isValid = true;
 
-        [...this.formEl.elements].forEach(function(field, index) {
+        [...this.formEl.elements].forEach((field, index) => {
+
+            if (['descricao', 'quantidade'].indexOf(field.name) > -1 && !field.value) {
+
+                field.classList.add('is-invalid');
+                return this.addLine() = false;
+
+            }
 
             if (field.name) {
                 item[field.name] = field.value;
@@ -37,16 +46,18 @@ class ItemController {
     }
 
     addLine(dataItem) {
+
         this.tableEl.innerHTML = `
-        <tr>
-            <td>01</td>
-            <td>${dataItem.descricao}</td>
-            <td>${dataItem.quantidade}</td>
-            <td>
-            <button type="button " class="btn btn-danger btn-sm ">Excluir</button>
-            </td>
-        </tr>
-    `;
+            <tr>
+                <td>01</td>
+                <td>${dataItem.descricao}</td>
+                <td>${dataItem.quantidade}</td>
+                <td>
+                <button type="button " class="btn btn-danger btn-sm ">Excluir</button>
+                </td>
+            </tr>
+        `;
+
 
     }
 
